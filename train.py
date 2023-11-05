a9b470549278e82c7b4d8a9b69e47844ded5c73a
shichuanyes <shichuanyes@gmail.com>
2023-11-04 22:27:16 -0400

    Add `train_test_split` to avoid `sklearn`

--- train.py
+++ train.py
@@ -2,14 +2,13 @@ import argparse
 
 import pandas as pd
 import torch
-from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from dataset import RNADataset
 from model import RNAModel
-from utils import nucleotides, mae
+from utils import nucleotides, mae, train_test_split
 
 
 def train(
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import math
+
+import numpy as np
+import pandas as pd
 import torch
 
 max_seq_length = 206
@@ -11,3 +15,15 @@ def str_to_seq(s):
 
 def mae(outputs, labels):
     return torch.mean(torch.abs(outputs - labels))
+
+
+def train_test_split(
+        df: pd.DataFrame,
+        test_size: float,
+        random_state: int
+):
+    n = len(df)
+    rng = np.random.default_rng(seed=random_state)
+    idx = rng.permutation(n)
+    split = math.floor(n * test_size)
+    return df.iloc[idx[:split]], df.iloc[idx[split:]]
